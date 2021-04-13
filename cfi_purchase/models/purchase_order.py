1838879f4b41e01955b2b8e6109468141364c8fd
Brian <brc@odoo.com>
2021-04-12 19:28:37 -0700

    [IMP]field on sale, purchase, production lines to connect related orders generated, updated views on sale purchase production to see related orders

---
+++ cfi_purchase/models/purchase_order.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    sale_order_ids = fields.Many2many(comodel_name='sale.order', compute='_compute_sale_order_ids', string='Sale Orders')
+
+    @api.depends('sale_order_count')
+    def _compute_sale_order_ids(self):
+        for purchase in self:
+            purchase.sale_order_ids = purchase._get_sale_orders()
