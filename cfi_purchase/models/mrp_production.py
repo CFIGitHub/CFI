0ce04c8601f915f795440379cea12f483500c57b
Brian <brc@odoo.com>
2021-05-20 00:53:40 -0700

    [FIX] error handle when sale_line_id is not passed to mrp/po

--- cfi_purchase/models/mrp_production.py
+++ cfi_purchase/models/mrp_production.py
@@ -17,10 +17,12 @@ class MrpProduction(models.Model):
 
     def _get_move_raw_values(self, product_id, product_uom_qty, product_uom, operation_id=False, bom_line=False):
         data = super(MrpProduction, self)._get_move_raw_values(product_id, product_uom_qty, product_uom, operation_id, bom_line)
-        data.update({'sale_line_id': self.sale_line_id[0].id if len(self.sale_line_id) else False})
+        if len(self.sale_line_id):
+            data.update({'sale_line_id': self.sale_line_id[0].id})
         return data
 
     def _get_move_finished_values(self, product_id, product_uom_qty, product_uom, operation_id=False, byproduct_id=False):
         data = super(MrpProduction, self)._get_move_finished_values(product_id, product_uom_qty, product_uom, operation_id, byproduct_id)
-        data.update({'sale_line_id': self.sale_line_id[0].id if len(self.sale_line_id) else False})
+        if len(self.sale_line_id):
+            data.update({'sale_line_id': self.sale_line_id[0].id})
         return data
